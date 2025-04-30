76651276088e5f06f3d83b2c3265cd82f4b37188
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2025-04-30 14:08:32 -0300

    :sparkles: implementando crud a um array de objetos

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,36 +1,33 @@
 package conta_bancaria;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import conta_bancaria.controller.ContaController;
 import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.util.Cores;
 
 public class Menu {
-	
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner read = new Scanner(System.in);
-		
-		int opcao;
-		
-		// instanciando um objeto da classe ContaCorrente
-		ContaCorrente cc1 = new ContaCorrente(2,456,1,"Renata Negrini", 600000, 60000);
-		cc1.visualizar();
-		
-		
-		cc1.sacar(661000);
-		cc1.visualizar();
+
+		ContaController contas = new ContaController();
+
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
 		
 		
-		ContaPoupanca cp1 = new ContaPoupanca(3,456,2,"Lucas Manrick", 1000000,3);
-		cp1.visualizar();
-		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000.00f, 100.00f);
+		contas.cadastrar(cc1);
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 123, 2, "Maria da Silva", 1000.00f, 12);
+		contas.cadastrar(cp1);
+
 		while (true) {
-			System.out.println(Cores.TEXT_WHITE_BOLD + Cores.ANSI_BLACK_BACKGROUND );
+			System.out.println(Cores.TEXT_WHITE_BOLD + Cores.ANSI_BLACK_BACKGROUND);
 			System.out.println("***************************************************");
 			System.out.println("****                                           ****");
 			System.out.println("******                                       ******");
@@ -51,20 +48,116 @@ public class Menu {
 			System.out.println("****   	|_________________________________|    ****");
 			System.out.println("***************************************************");
 			System.out.println(Cores.TEXT_RESET);
-			
+
 			opcao = read.nextInt();
+
+			if (opcao == 9) {
+				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				read.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 0: System.out.println("Saindo do sistema");
+				read.close();
+			case 1:
+				System.out.println("Criar Conta\n\n");
+
+				System.out.println("Digite o numero da agencia: ");
+				agencia = read.nextInt();
+
+				System.out.println("Digite o nome do titular: ");
+				read.skip("\\R");
+				titular = read.nextLine();
+
+				System.out.println("Digite o tipo da conta (1 - CC | 2 - CP): ");
+				tipo = read.nextInt();
+
+				System.out.println("Digite Saldo inicial da conta: ");
+				saldo = read.nextFloat();
+
+				switch (tipo) {
+				case 1:
+					System.out.println("Digite o limite da conta: ");
+					limite = read.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia,tipo,titular, saldo, limite));
+					break;
+				case 2:
+					System.out.println("Digite o dia aniversario da conta: ");
+					aniversario = read.nextInt();
+					contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia,tipo,titular, saldo, aniversario));
+				default:
+					System.out.println("tipo de conta invalida, selecione uma opção valida.");
+				}
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println("Listar todas as Contas\n\n");
+				contas.listarTodas();
+				keyPress();
+				break;
+			case 3:
+				System.out.println("Consultar dados da Conta - por número\n\n");
+				
+				System.out.println("Digite o numero da conta que você deseja consultar os dados: ");
+				try {
+				contas.procurarPorNumero(read.nextInt());
+				} catch (ArithmeticException e) {
+					System.out.println("digite um numero valido!");
+				}
+				keyPress();
+				break;
+			case 4:
+				System.out.println("Atualizar dados da Conta\n\n");
+				keyPress();
+				break;
+			case 5:
+				System.out.println("Apagar a Conta\n\n");
+				keyPress();
+				break;
+			case 6:
+				System.out.println("Saque\n\n");
+				keyPress();
+				break;
+			case 7:
+				System.out.println("Depósito\n\n");
+				keyPress();
+				break;
+			case 8:
+				System.out.println("Transferência entre Contas\n\n");
+				keyPress();
+				break;
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				keyPress();
+				break;
+			}
+
 			
-			read.close();
-			
+
 		}
-		
-		
 
 	}
 
-	
-	
-	public static void sobre () {
-		System.out.println("Este é o projeto 'conta bancaria'\ndesenvolvido por: Lucas Manrick Teodoro da Fonseca! \ngithub:https://github.com/lucasmanrick\nlinkedin:https://www.linkedin.com/in/lucas-manrick-0310b3228/");
+	public static void sobre() {
+		System.out.println(
+				"Este é o projeto 'conta bancaria'\ndesenvolvido por: Lucas Manrick Teodoro da Fonseca! \ngithub:https://github.com/lucasmanrick\nlinkedin:https://www.linkedin.com/in/lucas-manrick-0310b3228/");
 	}
+
+	public static void keyPress() {
+
+		try {
+
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.err.println("Ocorreu um erro ao tentar ler o teclado");
+
+		}
+	}
+
 }
--- src/conta_bancaria/controller/ContaController.java
+++ src/conta_bancaria/controller/ContaController.java
@@ -1,26 +1,45 @@
 package conta_bancaria.controller;
 
+import java.util.ArrayList;
+
 import conta_bancaria.model.Conta;
 import conta_bancaria.repository.ContaRepository;
 
 public class ContaController implements ContaRepository {
+	
+	//Criar a Collection Arraylist para armazenar as informações da conta (array de objetos).
+	
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>(); // funcionará como se fosse nosso banco de dados.
+	
+	//Variável para controlar os números das contas (id das contas).
+	
+	int numero = 0;
 
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+		
+		if(conta != null) {
+			conta.visualizar();
+		}else {
+			System.out.printf("\n A conta %d não foi encontrada", numero);
+		}
 		
 	}
 
 	@Override
 	public void listarTodas() {
-		// TODO Auto-generated method stub
+		for(var conta : listaContas) {
+			conta.visualizar();  //para cada conta que tiver no arraylist, vamos usar o metodo visualizar que já existe em cada conta.
+			
+		}
 		
 	}
 
 	@Override
 	public void cadastrar(Conta conta) {
-		// TODO Auto-generated method stub
-		
+		listaContas.add(conta);
+		System.out.println("Conta criada com sucesso!");
 	}
 
 	@Override
@@ -51,6 +70,21 @@ public class ContaController implements ContaRepository {
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	//Métodos Auxiliares
+	
+	public int gerarNumero() {
+		return ++ numero;
+	}
+	
+	public Conta buscarNaCollection (int numero) {
+		for(var conta : listaContas) {
+			if(conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		return null;
 	}
 
 }
